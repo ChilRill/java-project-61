e1f2ebd447744bc31aab8f12958f892163dde38d
kirill <kirillgerasimchik99@gmail.com>
2025-05-14 22:37:17 +0900

    12

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,28 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greetUser();
+        Scanner menuScanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Great");
+        System.out.println("0 - Exit");
+
+        System.out.println("Your choice: ");
+        String userChoice = menuScanner.nextLine();
+
+        switch (userChoice) {
+            case "1":
+                Cli.greetUser();
+                break;
+            case "0":
+                System.out.println("Exiting...");
+                break;
+            default:
+                System.out.println("Invalid choice. Please select a valid game number.");
+        }
+        menuScanner.close();
     }
 }
