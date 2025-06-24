b2ac7d318749506f4e4d514a5fc4e8ce6538a681
kirill <kirillgerasimchik99@gmail.com>
2025-06-24 15:53:07 +0900

    11

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -6,13 +6,16 @@ import java.util.Random;
 public class Calc {
     private static final Random RANDOM = new Random();
 
+    private static final int MAX_RANDOM_NUMBER = 40;
+    private static final int GAME_DATA = 2;
+
     public static void play() {
         String gameDescription = "What is the result of the expression?";
-        String[][] gameData = new String[Engine.ROUNDS][2];
+        String[][] gameData = new String[Engine.ROUNDS][GAME_DATA];
 
         for (int i = 0; i < Engine.ROUNDS; i++) {
-            int num1 = RANDOM.nextInt(40);
-            int num2 = RANDOM.nextInt(40);
+            int num1 = RANDOM.nextInt(MAX_RANDOM_NUMBER);
+            int num2 = RANDOM.nextInt(MAX_RANDOM_NUMBER);
             char[] operations = {'+', '-', '*'};
             char operation = operations[RANDOM.nextInt(operations.length)];
 
